ead73ba0a2514da23a2ef81dbe0c1e8c3c1bb16f
michael-p37 <michaelp_99@outlook.com>
2026-06-03 15:40:08 -0700

    feat: seed updated

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,13 +1,13 @@
 import "dotenv/config";
-import { PrismaClient } from "@prisma/client";
+import { Role } from "@prisma/client";
+import  {prisma} from"../src/db/prisma.js";
 import { hashPassword } from "../src/utils/auth.js";
-
-const prisma = new PrismaClient();
 
 await prisma.user.create({
   data: {
+    name: "AdminUser",
     email: "admin@test.com",
     password: await hashPassword("admin123"),
-    role: "ADMIN"
+    role: Role.ADMIN,
   }
 });
