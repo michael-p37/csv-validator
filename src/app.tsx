4ec96503b6e06d683ee35023e13324e3eeb2fca2
michael-p37 <michaelp_99@outlook.com>
2026-06-02 20:33:25 -0700

    feat: add session setup

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,13 @@
 import express from "express";
 import { renderToString } from "react-dom/server";
 import {LoginPage} from "./pages/LoginPage.js";
+import { sessionConfig } from "./config/session.js";
 
 const app = express();
 
 app.use(express.json());
+
+app.use(sessionConfig)
 
 app.get("/", (req, res) => {
   const html = renderToString(
---
+++ src/config/session.ts
@@ -0,0 +1,6 @@
+import session from "express-session";
+ export const sessionConfig = session({
+    secret: process.env.SESSION_SECRET!,
+    resave: false,
+    saveUninitialized: false,
+ })
