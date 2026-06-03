60da8ffd914a9dc30fa7c8629f54e9042180de29
michael-p37 <michaelp_99@outlook.com>
2026-06-02 20:40:24 -0700

    feat: add zod setup

---
+++ src/schemas/auth.schema.ts
@@ -0,0 +1,6 @@
+import { email, z } from "zod";
+
+export const loginSchema = z.object({
+    email: z.email().min(1, "Email es requerido"),
+    password: z.string().min(1, "Password es requerido"),
+});
