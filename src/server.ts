63f44bbae465cdd1dbfa7ccf302478f6ebabd90f
michael-p37 <michaelp_99@outlook.com>
2026-06-04 10:10:02 -0700

    feat: updating page middleware

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
 import appRoutes from "./routes/app.routes.js";
-import { render } from "./renderApp.js";
+import fs from "fs";
+import path from "path";  
 import { sessionConfig } from "./config/session.js";
+import { render } from "./entry-server.js";
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -17,9 +19,27 @@ app.use(sessionConfig)
 app.use(authRoutes);
 app.use(appRoutes);
 
-app.get("/{*splat}", (req, res) => {
-  const html = render(req.url)
-  res.send(html);
+app.get("/{*splat}", async (req, res) => {
+   const html = await render(req.url);
+
+  // si es Response (redirect, etc)
+  if (html instanceof Response) {
+    res.status(html.status);
+
+    const location = html.headers.get("Location");
+    if (location) {
+      return res.redirect(location);
+    }
+
+    return res.end();
+  }
+
+  // aquí ya es string seguro
+  const template = fs.readFileSync( path.resolve("index.html"), "utf-8" );
+
+  const finalHtml = template.replace(`<!--app-html-->`, html);
+  
+  res.status(200).set({ "Content-Type": "text/html" }).send(finalHtml);
 });
 
 app.listen(PORT, () => {
