7ca1f82a7e9b158d78b9fd8fc132342d07aa7ca6
michael-p37 <michaelp_99@outlook.com>
2026-06-03 22:35:12 -0700

    frefactor: Integrating the content of app.ts into server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,26 @@
-import app from "./app.js";
+import express from "express";
+import authRoutes from "./routes/auth.routes.js";
+import appRoutes from "./routes/app.routes.js";
+import { render } from "./renderApp.js";
+import { sessionConfig } from "./config/session.js";
 const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static("dist/client"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(sessionConfig)
+
+app.use(authRoutes);
+app.use(appRoutes);
+
+app.get("/{*splat}", (req, res) => {
+  const html = render(req.url)
+  res.send(html);
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
