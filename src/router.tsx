af7f55cc5ef21c21ae4a4d1960c4f71df399e34a
michael-p37 <michaelp_99@outlook.com>
2026-06-04 09:54:24 -0700

    feat: defining page routes

---
+++ src/router.tsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter, type RouteObject } from "react-router-dom";
+
+import { HomePage } from "./pages/HomePage";
+import { LoginPage } from "./pages/LoginPage";
+import { DashboardPage } from "./pages/DashboardPage";
+import { ErrorPage } from "./pages/ErrorPage";
+import AppLayout from "./AppLayout";
+
+export const routes = ([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "login", element: <LoginPage /> },
+      { path: "dashboard", element: <DashboardPage /> }
+    ]
+  }
+]);
--- src/routes/app.routes.ts
+++ src/routes/app.routes.ts
@@ -1,24 +1,18 @@
+import { render } from "@/entry-server";
 import { requireAuth } from "@/middlewares/auth.middleware.js";
 import { requireRole } from "@/middlewares/role.middleware.js";
-import { render } from "@/renderApp";
 import { Router } from "express";
 
 const router = Router();
 
-// 🔓 pública
-router.get("/", (req, res) => {
-  const html = render(req.url);
-  res.send(html);
-});
-
 // 🔒 usuario logueado
-router.get("/dashboard", requireAuth, (req, res) => {
+router.get("/dashboard", requireAuth, async (req, res) => {
   const html = render(req.url);
   res.send(html);
 });
 
 // 👑 solo admin
-router.get( "/admin", requireAuth, requireRole("ADMIN"), (req, res) => {
+router.get( "/admin", requireAuth, requireRole("ADMIN"), async (req, res) => {
     const html = render(req.url);
     res.send(html);
   }
